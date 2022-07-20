7280a037b482c87409b2269d03e5e77bc07b0fb1
Crafterzilla <osmarrojas69@gmail.com>
2022-07-19 21:31:17 -0400

    Add player animation

--- src/player.cpp
+++ src/player.cpp
@@ -6,14 +6,19 @@ Player::Player(Texture2D sprite) {
     this->y = 625;
     this->pacman = {x, y, 25, 25};
     this->currentDirection = nodir;
+    this->timer = 0.0f;
+    this->frame = 0;
 };
 
 void Player::DrawPlayer() {
     Vector2 position = {x, y};
     const int scaleFactor = 25;
-    static float rotation = 1;
-    static Rectangle pacFrame = {0, 25, static_cast<float>(pacmanSprites.width / 4), 
-    static_cast<float>(pacmanSprites.height / 4)};
+    static float rotation = 1.0f;
+
+    const float frameWidth = static_cast<float>(pacmanSprites.width / 4),
+    frameHeight = static_cast<float>(pacmanSprites.height / 4);
+
+    static Rectangle pacFrame = {static_cast<float>(frame), 25, frameWidth, frameHeight};
 
     if (currentDirection == up) {
         rotation = scaleFactor * up;
@@ -27,8 +32,17 @@ void Player::DrawPlayer() {
     else if (currentDirection == right) {
         rotation = scaleFactor * right;
     }
-    pacFrame = {0, rotation, static_cast<float>(pacmanSprites.width / 4), 
-    static_cast<float>(pacmanSprites.height / 4)};
+
+    timer += GetFrameTime();
+    if (timer >= 0.05) {
+        timer = 0.0f;
+        frame += 1;
+    }
+
+    frame %= 4;
+
+    pacFrame = {static_cast<float>(frame) * scaleFactor, rotation, frameWidth, 
+    frameHeight};
 
     pacman = {x, y, 25, 25};
     DrawTextureRec(pacmanSprites, pacFrame, position, RAYWHITE);
