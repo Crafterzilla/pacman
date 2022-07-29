df3b113a52ea21a70b9491aa26680e8922ebc63c
Crafterzilla <osmarrojas69@gmail.com>
2022-07-29 14:37:37 -0400

    Fix more script errors for deb linux

--- buildOnDebianLinux.sh
+++ buildOnDebianLinux.sh
@@ -3,20 +3,16 @@
 sudo apt update && sudo apt upgrade
 
 #Install some other important tools
-sudo apt install git-all
-sudo apt install build-Essentials
-sudo apt install libasound2-dev mesa-common-dev libx11-dev libxrandr-dev libxi-dev xorg-dev libgl1-mesa-dev libglu1-mesa-dev
-sudo apt install cmake
-
+sudo apt install git-all -y
+sudo apt install build-Essentials -y
+sudo apt install libasound2-dev mesa-common-dev libx11-dev libxrandr-dev libxi-dev xorg-dev libgl1-mesa-dev libglu1-mesa-dev mesa-utils -y
 
 #install raylib
 cd ~/Documents
 
 git clone https://github.com/raysan5/raylib.git raylib
-cd raylib
-mkdir build && cd build
-cmake -DBUILD_SHARED_LIBS=ON ..
-make
+cd raylib/src/
+make PLATFORM=PLATFORM_DESKTOP
 sudo make install
 sudo ldconfig
 
